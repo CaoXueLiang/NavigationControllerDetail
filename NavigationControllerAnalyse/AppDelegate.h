42d5b583fd4e9a4b99b9a87f5fff65be751fcae1
曹学亮 <2357747972@qq.com>
2017-12-15 18:42:22 +0800

    备份

---
+++ NavigationControllerAnalyse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationControllerAnalyse
+//
+//  Created by bjovov on 2017/12/15.
+//  Copyright © 2017年 caoxueliang.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
